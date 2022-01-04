bd86d459abe458ef1b21fb656e7aa7c35f7550df
ruofan <tinawei@damaiapp.com.tw>
2022-01-04 17:16:55 +0800

    learn the diferent with immutable and mutable variable

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
+// use std::io library
 use std::io;
+
+// declares a new function
+fn main(){
+    // print
+    println!("guess number");
+    println!("input guess");
+
+    // create a place to store the user input
+    // use mut before variable make a variable mutable
+    // immutable variable can not be assign twice
+    let mut guess = String::new();
+
+
+}
