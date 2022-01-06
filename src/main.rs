405f8363e2d0f23b15a43bff8531fe414825ba27
ruofan <tinawei@damaiapp.com.tw>
2022-01-06 11:02:12 +0800

    feat: test the first part

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,19 @@ fn main(){
     // mutable means allow the value be change
     // different with mut and shadow - mut not allow to mutate a variable type
     // shadow - use keyword of let, we can change the type of value, and reuse the same variable
+    // String::new - a function return a new, empty instance of string
     let mut guess = String::new();
 
+    // stdin function from io module, return an instance of std::io::Stdin
+    io::stdin()
+        // read_line method - get input from user
+        // job for read_line take user type into input and append a string
+        // & - indicates reference, reference are immutable by default
+        .read_line(&mut guess)
+        // handle potential failure with result
+        .expect("Failed to read line");
+
+    // {} is a placeholder, first set of curly brackets holds the first value, ans so on
+    println!("You guessed {}", guess );
 
 }
