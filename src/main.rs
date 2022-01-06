7ebf5a1d061abcedc99210888a956f74288d90ed
ruofan <tinawei@damaiapp.com.tw>
2022-01-06 10:39:12 +0800

    feat: add comment for diferent with mut and shadow

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ fn main(){
     // immutable variable can not be assign twice
     // variable is immutable by default
     // mutable means allow the value be change
+    // different with mut and shadow - mut not allow to mutate a variable type
+    // shadow - use keyword of let, we can change the type of value, and reuse the same variable
     let mut guess = String::new();
 
 
